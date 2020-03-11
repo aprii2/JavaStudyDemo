45c08f39a2dd8a3d4048535d33a7079807e58943
panyun01_sx <akise0412@163.com>
2020-03-11 23:54:36 +0800

    Random Test

---
+++ src/main/java/com/pan/map/MapTest.java
@@ -0,0 +1,25 @@
+package com.pan.map;
+
+public class MapTest {
+    /*
+    * 内容 - Map
+    *
+    * 散列表相关知识
+    * HashMap
+    *   内部实现
+    *   常用方法
+    * TreeMap
+    * */
+    /*
+    * 散列表 HashTable：也是哈希表。
+    * 根据数组，可以使用下标的方式来快速随机访问来实现的
+    * 比如：
+    * 存放一组数据：aaa，cbe，bfk，……
+    * List<String>
+    * 如何快速的定位和查找？
+    *
+    * String[] str;
+    * 哈希函数：将关键字转化成一个正整数
+    * aaa -> 256
+    * */
+}
